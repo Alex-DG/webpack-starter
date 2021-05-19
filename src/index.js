d12920656496809cd322863678bdcc9b725f33e8
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-05-19 15:49:22 +0100

    Setup Babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// Create a class property without a constructor
+class Game {
+  name = 'Violin Charades'
+}
+const myGame = new Game()
+// Create paragraph node
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.name}.`
+
 // Create heading node
 const heading = document.createElement('h1')
 heading.textContent = 'Interesting!'
 
-// Append heading node to the DOM
+// Append SVG and heading nodes to the DOM
 const app = document.querySelector('#root')
-app.append(heading)
+app.append(heading, p)
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  // Entry: what file or files webpack will look at to compile.
+  /**
+   * Entry: what file or files webpack will look at to compile.
+   */
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
-  // Where the bundled file will resolve. We'll have it output in the dist folder, which is where production code gets built. The [name] in the output will be main, as specified in the entry object.
+
+  /**
+   * Where the bundled file will resolve. We'll have it output in the dist folder, which is where production code gets built. The [name] in the output will be main, as specified in the entry object.
+   */
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
+
+  /**
+   * Webpack has a plugin interface that makes it flexible.
+   * Internal webpack code and third party extensions use plugins.
+   */
   plugins: [
     // Inject the script automatically in the HTML template page
     new HtmlWebpackPlugin({
@@ -22,4 +32,18 @@ module.exports = {
     // Clears out anything in the dist folder after each build. This is important to ensure no old data gets left behind.
     new CleanWebpackPlugin(),
   ],
+
+  /**
+   * Webpack uses loaders to preprocess files loaded via modules.
+   */
+  module: {
+    rules: [
+      // JavaScript
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'], // Transpile files with Babel and webpack.
+      },
+    ],
+  },
 }
