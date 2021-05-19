767b4a287276c8602789ee1045e297abac42c07e
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-05-19 15:37:09 +0100

    Setup HTML template file and inject the entry script to it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Interesting')
+// Create heading node
+const heading = document.createElement('h1')
+heading.textContent = 'Interesting!'
+
+// Append heading node to the DOM
+const app = document.querySelector('#root')
+app.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-/**
- * entry: what file or files webpack will look at to compile.
- * output: where the bundled file will resolve. We'll have it output in the dist folder, which is where production code gets built. The [name] in the output will be main, as specified in the entry object.
- */
 module.exports = {
+  // Entry: what file or files webpack will look at to compile.
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
+  // Where the bundled file will resolve. We'll have it output in the dist folder, which is where production code gets built. The [name] in the output will be main, as specified in the entry object.
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
+  plugins: [
+    // Inject the script automatically in the HTML template page
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'), // template file
+      filename: 'index.html', // output file
+    }),
+  ],
 }
